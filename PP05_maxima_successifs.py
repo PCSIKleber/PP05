4aa02b12a5590a2b3983f5caed19c2409e4eeed1
Jean-Julien Fleck <jeanjulien.fleck@gmail.com>
2018-12-13 18:27:50 +0100

    Initial commit

---
+++ PP05_maxima_successifs.py
@@ -0,0 +1,16 @@
+# Pour la lecture
+import numpy as np
+# Lecture des donnees
+donnees_t,donnees_u = np.loadtxt('Uc_41.txt',unpack=True)
+
+# Trouvez le moyen de determiner les valeurs de t (et de u) correspondant aux 
+# maxima successifs
+def temps_et_maxima(t,u):
+    liste_t,liste_u = [],[]
+    return liste_t,liste_u
+    
+    
+# Reste a afficher le resultat apres application de la fonction sur les donnees
+troisieme_temps_maxima = 'Attention, le troisieme est en position...'
+
+print(troisieme_temps_maxima)
